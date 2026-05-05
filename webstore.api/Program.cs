1e5a580b1cb088753e7336d8a5d4a85b480d696f
mrka75981 <magnusrk18@gmail.com>
2026-05-05 14:15:25 +0200

    updated to get a specific ID

--- webstore.api/Program.cs
+++ webstore.api/Program.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using webstore.api.Dtos;
 
@@ -6,14 +7,41 @@ var app = builder.Build();
 
 List<ItemDto> items = new List<ItemDto>(); 
 
-ItemDto Lasagne = new ItemDto
-{
-    ItemID = 1,
-    Name = "lasagne",
-    Price = 10.99m
-}; 
-items.Add(Lasagne);
+InitializeItems();
 
 app.MapGet("/items", () => items);
 
-app.Run();
+app.MapGet("/items/{id}", (int id) => items.Find(item => item.ItemID == id));
+
+app.Run(); 
+
+
+
+
+void InitializeItems()
+{
+    ItemDto Lasagne = new ItemDto
+    {
+        ItemID = 1,
+        Name = "lasagne",
+        Price = 10.99m
+    };  
+
+    ItemDto Bread = new ItemDto
+    {
+        ItemID = 2,
+        Name = "bread",
+        Price = 2.99m
+    }; 
+
+    ItemDto Spaghetti = new ItemDto
+    {
+        ItemID = 3,
+        Name = "spaghetti",
+        Price = 8.99m
+    };
+
+    items.Add(Lasagne);
+    items.Add(Bread);
+    items.Add(Spaghetti);
+}
