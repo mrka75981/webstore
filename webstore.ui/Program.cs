df00e2178641cff379e51a6199337135c0bcbc94
mrka75981 <magnusrk18@gmail.com>
2026-05-13 09:33:22 +0200

    Updated localhost but 'An unhandled error has occured' when launching

--- webstore.ui/Program.cs
+++ webstore.ui/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5168/items") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5169/") });
 
 await builder.Build().RunAsync();
